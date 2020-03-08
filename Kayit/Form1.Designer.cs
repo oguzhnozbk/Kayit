2f7c9c778e869ba0018070944aa511a0739ca29c
Oguzhan <oguzhnozbk@gmail.com>
2020-03-08 17:23:41 +0300

    Proje dosyası ekle.

---
+++ Kayit/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Kayit
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_tc = new System.Windows.Forms.TextBox();
+            this.label_tc = new System.Windows.Forms.Label();
+            this.label_ad = new System.Windows.Forms.Label();
+            this.textBox_ad = new System.Windows.Forms.TextBox();
+            this.label_soyad = new System.Windows.Forms.Label();
+            this.textBox_soyad = new System.Windows.Forms.TextBox();
+            this.button_kaydet = new System.Windows.Forms.Button();
+            this.button_kaydiSil = new System.Windows.Forms.Button();
+            this.comboBox_veri = new System.Windows.Forms.ComboBox();
+            this.button_guncelle = new System.Windows.Forms.Button();
+            this.dataGridView_tumkayitlar = new System.Windows.Forms.DataGridView();
+            this.label_data = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_tumkayitlar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox_tc
+            // 
+            this.textBox_tc.Location = new System.Drawing.Point(15, 25);
+            this.textBox_tc.Name = "textBox_tc";
+            this.textBox_tc.Size = new System.Drawing.Size(116, 20);
+            this.textBox_tc.TabIndex = 0;
+            // 
+            // label_tc
+            // 
+            this.label_tc.AutoSize = true;
+            this.label_tc.Location = new System.Drawing.Point(12, 9);
+            this.label_tc.Name = "label_tc";
+            this.label_tc.Size = new System.Drawing.Size(110, 13);
+            this.label_tc.TabIndex = 1;
+            this.label_tc.Text = "T.C. Kimlik Numarası :";
+            // 
+            // label_ad
+            // 
+            this.label_ad.AutoSize = true;
+            this.label_ad.Location = new System.Drawing.Point(12, 48);
+            this.label_ad.Name = "label_ad";
+            this.label_ad.Size = new System.Drawing.Size(31, 13);
+            this.label_ad.TabIndex = 2;
+            this.label_ad.Text = "İsim :";
+            // 
+            // textBox_ad
+            // 
+            this.textBox_ad.Location = new System.Drawing.Point(15, 64);
+            this.textBox_ad.Name = "textBox_ad";
+            this.textBox_ad.Size = new System.Drawing.Size(116, 20);
+            this.textBox_ad.TabIndex = 3;
+            // 
+            // label_soyad
+            // 
+            this.label_soyad.AutoSize = true;
+            this.label_soyad.Location = new System.Drawing.Point(12, 87);
+            this.label_soyad.Name = "label_soyad";
+            this.label_soyad.Size = new System.Drawing.Size(48, 13);
+            this.label_soyad.TabIndex = 4;
+            this.label_soyad.Text = "Soyisim :";
+            // 
+            // textBox_soyad
+            // 
+            this.textBox_soyad.Location = new System.Drawing.Point(15, 104);
+            this.textBox_soyad.Name = "textBox_soyad";
+            this.textBox_soyad.Size = new System.Drawing.Size(116, 20);
+            this.textBox_soyad.TabIndex = 5;
+            // 
+            // button_kaydet
+            // 
+            this.button_kaydet.Location = new System.Drawing.Point(15, 131);
+            this.button_kaydet.Name = "button_kaydet";
+            this.button_kaydet.Size = new System.Drawing.Size(116, 23);
+            this.button_kaydet.TabIndex = 6;
+            this.button_kaydet.Text = "Kaydet";
+            this.button_kaydet.UseVisualStyleBackColor = true;
+            this.button_kaydet.Click += new System.EventHandler(this.button_kaydet_Click);
+            // 
+            // button_kaydiSil
+            // 
+            this.button_kaydiSil.Location = new System.Drawing.Point(506, 52);
+            this.button_kaydiSil.Name = "button_kaydiSil";
+            this.button_kaydiSil.Size = new System.Drawing.Size(116, 23);
+            this.button_kaydiSil.TabIndex = 7;
+            this.button_kaydiSil.Text = "Kaydı Sil";
+            this.button_kaydiSil.UseVisualStyleBackColor = true;
+            this.button_kaydiSil.Click += new System.EventHandler(this.button_kaydiSil_Click);
+            // 
+            // comboBox_veri
+            // 
+            this.comboBox_veri.FormattingEnabled = true;
+            this.comboBox_veri.Location = new System.Drawing.Point(162, 25);
+            this.comboBox_veri.Name = "comboBox_veri";
+            this.comboBox_veri.Size = new System.Drawing.Size(460, 21);
+            this.comboBox_veri.TabIndex = 8;
+            this.comboBox_veri.SelectedIndexChanged += new System.EventHandler(this.comboBox_veri_SelectedIndexChanged);
+            // 
+            // button_guncelle
+            // 
+            this.button_guncelle.Location = new System.Drawing.Point(15, 161);
+            this.button_guncelle.Name = "button_guncelle";
+            this.button_guncelle.Size = new System.Drawing.Size(116, 23);
+            this.button_guncelle.TabIndex = 9;
+            this.button_guncelle.Text = "Güncelle";
+            this.button_guncelle.UseVisualStyleBackColor = true;
+            this.button_guncelle.Click += new System.EventHandler(this.button_guncelle_Click);
+            // 
+            // dataGridView_tumkayitlar
+            // 
+            this.dataGridView_tumkayitlar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_tumkayitlar.Location = new System.Drawing.Point(162, 81);
+            this.dataGridView_tumkayitlar.Name = "dataGridView_tumkayitlar";
+            this.dataGridView_tumkayitlar.Size = new System.Drawing.Size(460, 298);
+            this.dataGridView_tumkayitlar.TabIndex = 10;
+            // 
+            // label_data
+            // 
+            this.label_data.AutoSize = true;
+            this.label_data.Location = new System.Drawing.Point(159, 387);
+            this.label_data.Name = "label_data";
+            this.label_data.Size = new System.Drawing.Size(0, 13);
+            this.label_data.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 409);
+            this.Controls.Add(this.label_data);
+            this.Controls.Add(this.dataGridView_tumkayitlar);
+            this.Controls.Add(this.button_guncelle);
+            this.Controls.Add(this.comboBox_veri);
+            this.Controls.Add(this.button_kaydiSil);
+            this.Controls.Add(this.button_kaydet);
+            this.Controls.Add(this.textBox_soyad);
+            this.Controls.Add(this.label_soyad);
+            this.Controls.Add(this.textBox_ad);
+            this.Controls.Add(this.label_ad);
+            this.Controls.Add(this.label_tc);
+            this.Controls.Add(this.textBox_tc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_tumkayitlar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_tc;
+        private System.Windows.Forms.Label label_tc;
+        private System.Windows.Forms.Label label_ad;
+        private System.Windows.Forms.TextBox textBox_ad;
+        private System.Windows.Forms.Label label_soyad;
+        private System.Windows.Forms.TextBox textBox_soyad;
+        private System.Windows.Forms.Button button_kaydet;
+        private System.Windows.Forms.Button button_kaydiSil;
+        private System.Windows.Forms.ComboBox comboBox_veri;
+        private System.Windows.Forms.Button button_guncelle;
+        private System.Windows.Forms.DataGridView dataGridView_tumkayitlar;
+        private System.Windows.Forms.Label label_data;
+    }
+}
+
---
+++ Kayit/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace Kayit
+{
+    public partial class Form1 : Form
+    {
+        void VeriEkle(string key, string tc, string ad, string soyad)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+            string komut =
+            "open symmetric key [SimetrikDenemeAnahtari] decryption by certificate [SifrelemeSertifikasi]   " +
+            "declare @KeyGuid as uniqueidentifier   " +
+            "set @KeyGuid = key_guid('" + key + "')  " +
+            "insert into kayit_islemi ( tc , ad , soyad )   " +
+            "values ( " +
+            "encryptbykey(@KeyGuid,N'" + tc + "'),   " +
+            "encryptbykey(@KeyGuid,N'" + ad + "'),   " +
+            "encryptbykey(@KeyGuid,N'" + soyad + "'))  " +
+            "close symmetric key [SimetrikDenemeAnahtari]  ";
+            SqlCommand sorgu = new SqlCommand(komut, baglanti);
+            try
+            {
+                baglanti.Open();
+                sorgu.ExecuteNonQuery();
+                MessageBox.Show("Kayıt Eklendi");
+                baglanti.Close();
+                comboBox_veri.Items.Add(tc);
+            }
+            catch
+            {
+                MessageBox.Show("Ekleme işlemi Yapılamadı. Lütfen bilgileri kontrol edip tekrar deneyiniz.");
+            }
+        }
+        void KayitGetir()
+        {
+            SqlConnection baglanti;
+            SqlDataAdapter dAdapter;
+            try
+            {
+                dataGridView_tumkayitlar.DataSource = null;
+                baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+                baglanti.Open();
+                string komut = "select * from dbo.kayit_islemi";
+                dAdapter = new SqlDataAdapter(komut, baglanti);
+                DataSet dSet = new DataSet();
+                dAdapter.Fill(dSet, "Kayıtlar");
+                if (dSet.Tables[0].Rows.Count == 0)
+                {
+                    label_data.Text = "Kayit Bulunamadı.";
+                    return;
+                }
+                else
+                {
+                    label_data.Text = dSet.Tables[0].Rows.Count + " adet kayıt getirildi.";
+                    dataGridView_tumkayitlar.DataSource = dSet.Tables["Kayıtlar"];
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Hata : " + ex);
+
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+            SqlDataAdapter adaptor = new SqlDataAdapter("select * from kayit_islemi", baglanti);
+            DataSet ds = new DataSet();
+            adaptor.Fill(ds, "kayit_islemi");
+            int toplamkayit = ds.Tables["kayit_islemi"].Rows.Count;
+            baglanti.Close();
+
+            SqlCommand komut = new SqlCommand();
+            komut.CommandText = "select * from kayit_islemi";
+            komut.Connection = baglanti;
+            komut.CommandType = CommandType.Text;
+            SqlDataReader dr;
+            baglanti.Open();
+            dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox_veri.Items.Add(dr["tc"]);
+            }
+            baglanti.Close();
+            KayitGetir();
+        }
+
+        private void button_kaydet_Click(object sender, EventArgs e)
+        {
+            //VeriEkle("SimetrikDenemeAnahtari", textBox_tc.Text, textBox_ad.Text, textBox_soyad.Text);
+            string tc = textBox_tc.Text;
+            string ad = textBox_ad.Text;
+            string soyad = textBox_soyad.Text;
+            SqlConnection baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+            string komut = "insert into kayit_islemi (tc,ad,soyad) values ('" + tc + "','" + ad + "','" + soyad + "')";
+            SqlCommand sorgu = new SqlCommand(komut, baglanti);
+            try
+            {
+                baglanti.Open();
+                sorgu.ExecuteNonQuery();
+                MessageBox.Show("Kayıt Eklendi");
+                baglanti.Close();
+                comboBox_veri.Items.Add(tc);
+            }
+            catch
+            {
+                MessageBox.Show("Ekleme işlemi Yapılamadı. Lütfen bilgileri kontrol edip tekrar deneyiniz.");
+            }
+            KayitGetir();
+        }
+
+        private void button_kaydiSil_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+            string komut = "delete from kayit_islemi where  tc = '" + comboBox_veri.Text + "'";
+            try
+            {
+                baglanti.Open();
+                SqlCommand sorgu = new SqlCommand(komut, baglanti);
+                sorgu.ExecuteNonQuery();
+                baglanti.Close();
+                comboBox_veri.Items.RemoveAt(comboBox_veri.SelectedIndex);
+                MessageBox.Show("Kayıt silinmiştir.");
+                comboBox_veri.Text = null;
+                textBox_ad.Text = null;
+                textBox_soyad.Text = null;
+                textBox_tc.Text = null;
+
+            }
+            catch
+            {
+                MessageBox.Show("Kayıt silinemedi.");
+            }
+            KayitGetir();
+
+        }
+
+        private void comboBox_veri_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+            SqlDataAdapter adaptor = new SqlDataAdapter("select * from kayit_islemi", baglanti);
+            DataSet ds = new DataSet();
+            adaptor.Fill(ds, "kayit_islemi");
+            int toplamkayit = ds.Tables["kayit_islemi"].Rows.Count;
+            baglanti.Close();
+            DataRow kayit = ds.Tables["kayit_islemi"].Rows[comboBox_veri.SelectedIndex];
+            textBox_tc.Text = kayit.ItemArray.GetValue(1).ToString();
+            textBox_ad.Text = kayit.ItemArray.GetValue(2).ToString();
+            textBox_soyad.Text = kayit.ItemArray.GetValue(3).ToString();
+        }
+
+        private void button_guncelle_Click(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection("Data Source=FIZIKSELPC\\SQLEXPRESS; Initial Catalog=kayit; Integrated Security=true");
+            string komut = "update kayit_islemi set ad ='" + textBox_ad.Text + "' , soyad ='" + textBox_soyad.Text + "' where  tc = '" + comboBox_veri.Text + "'";
+            try
+            {
+                baglanti.Open();
+                SqlCommand sorgu = new SqlCommand(komut, baglanti);
+                sorgu.ExecuteNonQuery();
+                baglanti.Close();
+                MessageBox.Show("Kayıt güncellenmiştir.");
+
+            }
+            catch
+            {
+                MessageBox.Show("Kayıt güncellenemedi.");
+            }
+            KayitGetir();
+        }
+    }
+}
